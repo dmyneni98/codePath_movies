987295a5dd8f0d65c2ed1ee7e5a98438fd3405b2
dmyneni98 <mdhiraja@gmail.com>
2022-02-09 23:51:54 -0600

    Added movies files

---
+++ Movies/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  Movies
+//
+//  Created by Dhiraja Myneni on 2/9/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
